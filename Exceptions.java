5adf5b86878984b5c0201d1b9939ee12473ebbfc
Duong (Zach) Nguyen <novak@novakcgx.me>
2022-12-03 18:09:11 -0500

    Add exceptions/exit codes

---
+++ Exceptions.java
@@ -0,0 +1,6 @@
+/** A class containing all exit codes of the program. */
+class Exceptions {
+  static final int INVALID_COMMAND_LINE_ARGUMENTS = 1;
+  static final int FILE_NOT_FOUND = 2;
+  static final int INVALID_FILE_FORMAT = 3;
+}
