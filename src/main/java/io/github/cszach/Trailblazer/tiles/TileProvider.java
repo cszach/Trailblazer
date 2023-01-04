9fc185616a37fb4db53aada76a7e0d4b7f16d4e5
Duong (Zach) Nguyen <novak@novakcgx.me>
2023-01-04 17:47:30 -0500

    tiles: Delete TileProvider.java

--- src/main/java/io/github/cszach/Trailblazer/tiles/TileProvider.java
+++
@@ -1,9 +0,0 @@
-package io.github.cszach.Trailblazer.tiles;
-
-import java.util.Optional;
-
-public interface TileProvider {
-  public static Optional<String> getURLTemplate(int provider);
-
-  private static boolean requiresSubdomain(int provider);
-}
